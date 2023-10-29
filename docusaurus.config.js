80393e2fe928c46e72d420ad2ea8b3655146d4a0
xxxmammom2410 <akira2410.luminous@gmail.com>
2023-10-29 16:12:50 +0900

    fix:production用urlを修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://wac2w.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/WEBGL2_Docs/",
